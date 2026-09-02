8db9105072e8dd97479e979f5d7e8fb01c634caa
Rune Moskvil Lyngås <runely@users.noreply.github.com>
2026-09-02 12:08:59 +0200

    minor: 1.1.0 - remove-metrics (#3)
    * chore: type cleanup
    
    * feat: Added support for removing a metric
    
    * chore: Use double quotes and arrow functions
    
    * fix: Added documentation remove functions
    
    * chore: lint fix
    
    * chore: bump version to 1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import metricsClient from "prom-client";
+import metricsClient, { type LabelValues } from "prom-client";
+
+export type MetricLabel = [labelName: string, labelValue: string];
+
+type MetricType = "Counter" | "Gauge";
 
 const register = new metricsClient.Registry();
 metricsClient.collectDefaultMetrics({ register });
@@ -11,9 +15,9 @@ const gauges: Record<string, metricsClient.Gauge> = {};
  * If labels are provided, they must be in pairs of label name and label value.<br /><br />
  *
  * Example usage:
- * ```typescript
- * countInc('http_requests_total', 'Total number of HTTP requests', 5, ['method', 'GET'], ['status', '200'])
- * countInc('errors_total', 'Total number of errors', 1)
+ * ```TypeScript
+ * countInc("http_requests_total", "Total number of HTTP requests", 5, ["method", "GET"], ["status", "200"])
+ * countInc("errors_total", "Total number of errors", 1)
  * ```
  *
  * @param name - The name of the counter metric
@@ -21,7 +25,7 @@ const gauges: Record<string, metricsClient.Gauge> = {};
  * @param increment - The amount to increase the counter by
  * @param labels - Optional pairs of label names and label values
  */
-export function countInc(name: string, description: string, increment: number, ...labels: [labelName: string, labelValue: string][]): void {
+export const countInc = (name: string, description: string, increment: number, ...labels: MetricLabel[]): void => {
   const counter = counters[name] ?? createCounter(name, description, ...labels);
 
   if (labels.length > 0) {
@@ -30,34 +34,34 @@ export function countInc(name: string, description: string, increment: number, .
   }
 
   counter.inc(increment);
-}
+};
 
 /**
  * Increases a counter metric by 1. If the counter does not exist, it will be created with the provided <b>name</b>, <b>description</b>, and optional <i>labels</i>.<br />
  * If labels are provided, they must be in pairs of label name and label value.<br /><br />
  *
  * Example usage:
- * ```typescript
- * count('http_requests_total', 'Total number of HTTP requests', ['method', 'GET'], ['status', '200'])
- * count('errors_total', 'Total number of errors')
+ * ```TypeScript
+ * count("http_requests_total", "Total number of HTTP requests", ["method", "GET"], ["status", "200"])
+ * count("errors_total", "Total number of errors")
  * ```
  *
  * @param name - The name of the counter metric
  * @param description - A brief description of the counter metric
  * @param labels - Optional pairs of label names and label values
  */
-export function count(name: string, description: string, ...labels: [labelName: string, labelValue: string][]): void {
+export const count = (name: string, description: string, ...labels: MetricLabel[]): void => {
   countInc(name, description, 1, ...labels);
-}
+};
 
 /**
  * Sets a gauge metric to a specified value. If the gauge does not exist, it will be created with the provided <b>name</b>, <b>description</b>, and optional <i>labels</i>.<br />
  * If labels are provided, they must be in pairs of label name and label value.<br /><br />
  *
  * Example usage:
- * ```typescript
- * gauge('memory_usage_bytes', 'Memory usage in bytes', 512000, ['service', 'auth'])
- * gauge('active_sessions', 'Number of active sessions', 120)
+ * ```TypeScript
+ * gauge("memory_usage_bytes", "Memory usage in bytes", 512000, ["service", "auth"])
+ * gauge("active_sessions", "Number of active sessions", 120)
  * ```
  *
  * @param name - The name of the gauge metric
@@ -65,7 +69,7 @@ export function count(name: string, description: string, ...labels: [labelName: 
  * @param value - The value to set the gauge to
  * @param labels - Optional pairs of label names and label values
  */
-export function gauge(name: string, description: string, value: number, ...labels: [labelName: string, labelValue: string][]): void {
+export const gauge = (name: string, description: string, value: number, ...labels: MetricLabel[]): void => {
   const gauge = gauges[name] ?? createGauge(name, description, ...labels);
 
   if (labels.length > 0) {
@@ -74,12 +78,50 @@ export function gauge(name: string, description: string, value: number, ...label
   }
 
   gauge.set(value);
-}
+};
 
-const createCounter = (name: string, description: string, ...labels: [labelName: string, labelValue: string][]): metricsClient.Counter => {
-  if (labels.some((labelPair) => !Array.isArray(labelPair) || labelPair.length !== 2)) {
+/**
+ * Remove a counter metric from the Prometheus register.<br /><br />
+ *
+ * Example usage:
+ * ```TypeScript
+ * // if no labels are present for this metric
+ * removeCounter("http_requests_total")
+ *
+ * // when labels are present for this metric
+ * removeCounter("http_requests_total", ["method", "GET"])
+ * ```
+ *
+ * @param name - The name of the counter metric
+ * @param labels - Optional pairs of label names and label values
+ */
+export const removeCounter = (name: string, ...labels: MetricLabel[]): void => {
+  removeMetric("Counter", name, ...labels);
+};
+
+/**
+ * Remove a gauge metric from the Prometheus register.<br /><br />
+ *
+ * Example usage:
+ * ```TypeScript
+ * // if no labels are present for this metric
+ * removeGauge("active_sessions")
+ *
+ * // when labels are present for this metric
+ * removeGauge("active_sessions", ["service", "auth"])
+ * ```
+ *
+ * @param name - The name of the gauge metric
+ * @param labels - Optional pairs of label names and label values
+ */
+export const removeGauge = (name: string, ...labels: MetricLabel[]): void => {
+  removeMetric("Gauge", name, ...labels);
+};
+
+const createCounter = (name: string, description: string, ...labels: MetricLabel[]): metricsClient.Counter => {
+  if (labels.some((labelPair: MetricLabel) => !Array.isArray(labelPair) || labelPair.length !== 2)) {
     throw new Error(
-      `Can not create counter metric '${name}' with description '${description}' because labels must be provided in pairs of label name and label value!`
+      `Can not create counter metric "${name}" with description "${description}" because labels must be provided in pairs of label name and label value!`
     );
   }
 
@@ -95,10 +137,10 @@ const createCounter = (name: string, description: string, ...labels: [labelName:
   return counter;
 };
 
-const createGauge = (name: string, description: string, ...labels: [labelName: string, labelValue: string][]): metricsClient.Gauge => {
-  if (labels.some((labelPair) => !Array.isArray(labelPair) || labelPair.length !== 2)) {
+const createGauge = (name: string, description: string, ...labels: MetricLabel[]): metricsClient.Gauge => {
+  if (labels.some((labelPair: MetricLabel) => !Array.isArray(labelPair) || labelPair.length !== 2)) {
     throw new Error(
-      `Can not create gauge metric '${name}' with description '${description}' because labels must be provided in pairs of label name and label value!`
+      `Can not create gauge metric "${name}" with description "${description}" because labels must be provided in pairs of label name and label value!`
     );
   }
 
@@ -114,14 +156,41 @@ const createGauge = (name: string, description: string, ...labels: [labelName: s
   return gauge;
 };
 
-const generateLabelValues = (...labels: [labelName: string, labelValue: string][]): { [key: string]: string } => {
-  const labelValues: { [key: string]: string } = {};
+const generateLabelValues = (...labels: MetricLabel[]): LabelValues<string> => {
+  const labelValues: LabelValues<string> = {};
 
   labels.forEach(([labelName, labelValue]): void => {
     labelValues[labelName] = labelValue;
   });
 
   return labelValues;
+};
+
+const removeMetric = (type: MetricType, name: string, ...labels: MetricLabel[]): void => {
+  let metric: metricsClient.Metric | undefined;
+
+  switch (type) {
+    case "Counter":
+      metric = counters[name];
+      break;
+    case "Gauge":
+      metric = gauges[name];
+      break;
+    default:
+      throw new Error(`Metric type ${type} is not supported`);
+  }
+
+  if (!metric) {
+    throw new Error(`Metric with type ${type} was not found`);
+  }
+
+  if (labels.length > 0) {
+    metric.remove(generateLabelValues(...labels));
+    return;
+  }
+
+  metric.remove();
+  return;
 };
 
 export { register };
